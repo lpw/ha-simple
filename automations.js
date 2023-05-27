83c4fd26c405b112a6ed70afc1c608aac61f38c8
Lance Welsh <lance.welsh@wisk.aero>
2023-05-26 17:38:29 -0700

    init

---
+++ automations.js
@@ -0,0 +1,190 @@
+// allDay - removes sundown condition
+// nighttime - adds second automation with after midnight (and before sunrise?) condition with brightness to use
+// brightness - brightness level for all lights involved
+
+export const automations = [
+	{
+		name: 'Office Lights',
+		sensors: [ 'binary_sensor.office_ecolink_r12' ],
+		switches: [ 'light.office_lights' ],
+		// time: 3600,
+		// sun: false,
+		allDay: true,
+	},
+	{
+		name: 'Bar lights and lava lamp',
+		sensors: [ 'binary_sensor.game_nook_ecolink_r4_sensor_state_any', 'binary_sensor.bar_ecolink_n2_motion_detection' ],
+		switches: [ 'switch.bar_lights', 'light.stairtop_lights', 'switch.tv_lamps' ],
+		allDay: true,
+	},
+	{
+		name: 'MBR',
+		sensors: [ 'binary_sensor.mbr_ecolink_r8_sensor_state_any' ],
+		switches: [ 'light.bedroom_lights', 'light.bedroom_spotlight', 'light.deco_lamp' ],
+	},
+	{
+		name: 'MBA',
+		nighttime: 20,
+		sensors: [ 'binary_sensor.mba_ecolink_n3_motion_detection' ],
+		switches: [ 'light.nicolas_mirror_lights:50', 'light.lances_mirror_lights:100', 'light.deco_lamp' ],
+	},
+	{
+		name: 'DBR',
+		sensors: [],
+		switches: [ 'light.dbr_lamp' ],
+	},
+	{
+		name: 'QBR',
+		sensors: [ 'binary_sensor.qbr_ecolink_r3_sensor_state_any' ],
+		switches: [ 'light.qbr_lamp', 'light.qbr_light' ],
+	},
+	{
+		name: 'Blue room light',
+		sensors: [ 'binary_sensor.blue_room_ecolink_n1_motion_detection' ],
+		switches: [ 'light.blue_room_lamp', 'light.blue_room_light' ],
+	},
+	{
+		name: 'Dining Room',
+		nighttime: 20,
+		sensors: [ 'binary_sensor.dining_room_ecolink_b_r11_sensor_state_any' ],
+		switches: [ 'light.dining_room_lights', 'light.chandelier', 'light.living_room_lights', 'light.kitchen_lights', 'switch.entryway_christmas_lights' ],
+	},
+	{
+		name: 'Living Room',
+		sensors: [ 'binary_sensor.living_room_ecolink_r5_sensor_state_any' ],
+		switches: [ 'light.living_room_lights', 'light.dining_room_lights', 'light.kitchen_lights', 'switch.entryway_christmas_lights' ],
+	},
+	{
+		name: 'Kitchen',
+		sensors: [ 'binary_sensor.kitchen_ecolink_r2_sensor_state_any' ],
+		switches: [ 'light.kitchen_lights' ],
+	},
+	{
+		name: 'Garage Breezeway',
+		sensors: [ 'binary_sensor.garage_ecolink_r6_sensor_state_any' ],
+		switches: [ 'switch.garage_lights', 'switch.garage_stereo', 'switch.breezeway_lights' ],
+		allDay: true,
+	},
+	{
+		name: 'Garage Far',
+		sensors: [ 'binary_sensor.garage_ecolink_two_r21_sensor_state_any' ],
+		switches: [ 'switch.garage_lights', 'switch.garage_stereo', 'switch.garage_eve', 'light.garage_back_door_light' ],
+	},
+	{
+		name: 'Breezeway Garage',
+		sensors: [ 'binary_sensor.breezeway_ecolink_two_r22_sensor_state_any' ],
+		switches: [ 'switch.breezeway_lights', 'switch.garage_lights' ],
+	},
+	{
+		name: 'Breezeway Near',
+		sensors: [ 'binary_sensor.breezeway_ecolink_r7_sensor_state_any' ],
+		switches: [ 'switch.breezeway_lights', 'light.dog_room_lights' ],
+	},
+	{
+		name: 'Dog Room',
+		sensors: [ 'binary_sensor.dog_room_ecolink_dr1_motion_detection' ],
+		switches: [ 'light.dog_room_lights', 'light.dining_room_lights', 'switch.stereo' ],
+	},
+	{
+		name: 'Stairwell',
+		sensors: [ 'binary_sensor.stairwell_ecolink_r9_sensor_state_any' ],
+		switches: [ 'light.stairwell_light', 'light.stairtop_lights' ],
+	},
+	{
+		name: 'Bar and Game Nook and Stairwell',
+		sensors: [ 'binary_sensor.bar_ecolink_n2_motion_detection', 'binary_sensor.game_nook_ecolink_r4_sensor_state_any' ],
+		switches: [ 'light.stairwell_light', 'switch.bar_lights', 'light.blue_room_light', 'light.blue_room_lamp', 'light.qbr_lamp', 'light.qbr_lamp', 'switch.tv_lamps' ],
+	},
+	{
+		name: 'Nook',
+		sensors: [ 'binary_sensor.nook_ecolink_r10_sensor_state_any' ],
+		switches: [ 'light.nook_lights' ],
+	},
+	{
+		name: 'Foyer',
+		sensors: [ 'binary_sensor.foyer_ecolink_rn1_sensor_state_any' ],
+		switches: [ 'light.foyer' ],
+	},
+	{
+		name: 'Frontyard and Folly',
+		sensors: [ 'binary_sensor.porch_ecolink_motion_detection' ],
+		switches: [ 'light.porch', 'light.foyer', 'switch.bedroom_eve', 'switch.garage_eve', 'switch.folly' ],
+	},
+	{
+		name: 'Sideyard',
+		sensors: [],
+		switches: [ 'light.side_yard_flood_light' ],
+	},
+	{
+		name: 'Driveway',
+		sensors: [],
+		switches: [ 'switch.driveway_light' ],
+	},
+	{
+		name: 'Back Deck',
+		sensors: [ 'binary_sensor.back_deck_ecolink_bd1_motion_detection' ],
+		switches: [ 'switch.deck_lanterns', 'switch.deck_eve_lights', 'light.backyard_floodlight' ],
+	},
+	{
+		name: 'Off Heat Dish',
+		sensors: [],
+		time: 900,
+		switches: [ 'switch.heat_dish' ]
+	},
+	{
+		name: 'Offs',
+		sensors: [],
+		// don't include switch.heat_dish as it's above with shorter time 
+		// shower, bathtub, bathroom, bedside, skylight, ?? (already got sideyard, driveway, and dbr)
+		// do include gba, gbr, but remove on bedtime
+		// garage back door has trigger
+		switches: [ 
+			'light.gba_mirror_light',
+			'switch.gba_light',
+			'light.gbr_lights',
+			'light.shower_light',
+			'light.bathtub_light',
+			'switch.bathroom_lights',
+			'light.bedside_light',
+			'light.skylight',
+			'light.dbr_lamp',
+			'light.side_yard_flood_light',	// ?
+			'light.log_lights',
+			'light.living_room_spotlight',
+			'light.fridge_top_light',
+			'switch.door_2',
+			'switch.garage_stereo',
+			'switch.hot_water',
+			'switch.driveway_light',
+			'switch.fireplace',
+			'switch.porch_christmas_lights',
+			'switch.entryway_christmas_lights',
+			'switch.rv_heat',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+			'switch.christmas_tree_lights',
+		],
+	},
+]
+
+/*
+	{
+		name: 'Second bar lights and lava lamp',
+		sensors: [ 'binary_sensor.game_nook_ecolink_r4_sensor_state_any', 'binary_sensor.bar_ecolink_n2_motion_detection' ],
+		switches: [ 'switch.bar', 'light.other' ],
+		time: 3600,
+	},
+
+m1 -> s1 100
+m2 -> s1 100
+m2 -> s2 200
+m3 -> s1 300
+
+off -> s1 if m1,m2,s1.lu > 100
+off -> s1 if m3,s1.lu > 300
+*/
---
+++ generate.js
@@ -0,0 +1,288 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { namesToDeviceIds } from './entities.js'
+import { automations } from './automations.js'
+
+const getDeviceIdFromName = ( name ) => {
+	// console.log( 'getDeviceIdFromName name', name )
+	let deviceId = namesToDeviceIds[ name ]
+	// if( !deviceId ) {
+	// 	// console.log( 'LANCE trying _sensor_state_any for', name )
+	// 	name = `${name}_sensor_state_any`
+	// 	deviceId = namesToDeviceIds[ `${name}` ]
+	// }
+	if( !deviceId ) {
+		console.log( 'ERROR getDeviceIdFromName failed for', name )
+	}
+	return deviceId
+}
+
+// TODO: fix this
+const entityIdFromName = ( name ) => name.toLowerCase().replace( /\s/g, '_' )
+
+function getOnString( a, beforeSunrise, afterSunrise ) {
+	const nightSceneIdPrefix = beforeSunrise ? 'night_' : ''
+	const nightSceneNamePrefix = beforeSunrise ? 'Night ' : ''
+	const id = `id_on ${nightSceneIdPrefix}${a.name}`
+	const alias = `Turn on ${nightSceneNamePrefix}${a.name}`
+	const description = `Turn on ${nightSceneNamePrefix}${a.name}`
+	const scene = `scene_${a.name}`
+	const trigger = a.sensors.map( s => {
+		const deviceId = getDeviceIdFromName( s )
+		const ts = `
+  - type: turned_on
+    platform: device
+    device_id: ${deviceId}
+    entity_id: ${s}
+    domain: binary_sensor`
+		return ts
+	}).join('')
+	const notAllDayCondition = !a.allDay ? `
+  - condition: state
+    entity_id: sun.sun
+    state: below_horizon` : ''
+	const beforeSunriseCondition = beforeSunrise ? `
+  - condition: sun
+    before: sunrise` : ''
+	const afterSunriseCondition = afterSunrise ? `
+  - condition: sun
+    after: sunrise` : ''
+  const conditions = `${notAllDayCondition}${beforeSunriseCondition}${afterSunriseCondition}` || '[]'
+	// const action = a.switches.map( s => {
+	// 	const deviceId = getDeviceIdFromName( s )
+	// 	const as = `
+  // - type: turn_on
+  //   device_id: ${deviceId}
+  //   entity_id: ${s}
+  //   domain: ${s.split('.')[0]}`
+	// 	return as
+	// }).join('')
+	const action = `
+  - service: scene.turn_on
+    target: 
+      entity_id: scene.${nightSceneIdPrefix}${entityIdFromName(scene)}
+    metadata: {}`
+	const s = `
+- id: '${id}'
+  alias: ${alias}
+  description: '${description}'
+  trigger: ${trigger}
+  condition: ${conditions}
+  action: ${action}
+  mode: single
+`
+	return s
+}
+
+// function getSceneLight( lightString ) {
+// 	const ls = `
+//     ${light}:
+//       supported_color_models:
+//       - brightness
+//       friendly_name: ${light.split('.')[1]}
+//       supported_features: 32
+//       color_mode: brightness
+//       brightness: 255
+//       state: 'on'`
+// 	return ls
+// }
+
+function getSimpleSceneLight( lightString, nighttime ) {
+	const lightName = lightString.split( ':' )[ 0 ]
+	const brightnessDay = lightString.split( ':' )[ 1 ] || 255
+	const brightness = Math.round( nighttime ? brightnessDay * ( nighttime / 100 ) : brightnessDay )
+	// const domain = lightName.split( '.' )[ 0 ]
+	const friendlyName = lightName.split( '.' )[ 1 ]
+	const ls = `
+    ${lightName}:
+      friendly_name: ${friendlyName}
+      brightness: ${brightness}
+      state: 'on'`
+	return ls
+}
+
+function getSceneSwitch( sw ) {
+	const ls = `
+    ${sw}:
+      friendly_name: ${sw.split('.')[1]}
+      state: 'on'`
+	return ls
+}
+
+function getSceneString( a, nighttime ) {
+	const id = entityIdFromName( `${nighttime ? 'night ' : ''}scene ${a.name}` )
+	const name = `${nighttime ? 'Night ' : ''}Scene ${a.name}`
+	const entities = a.switches.map( s => s.split('.')[0] === 'light' ? getSimpleSceneLight( s, nighttime ) : getSceneSwitch( s ) ).join( '' )
+	const s = `
+- id: '${id}'
+  name: ${name}
+  entities: ${entities}
+  metadata: {}
+`
+	return s
+}
+
+// function getNightSceneString( a ) {
+// 	const id = entityIdFromName( `scene ${a.name}` )
+// 	const name = `Scene ${a.name}`
+// 	const entities = a.switches.map( s => s.split('.')[0] === 'light' ? getSimpleSceneLight( s ) : getSceneSwitch( s ) )
+// 	const s = `
+// - id: '${id}'
+//   name: ${name}
+//   entities: ${entities}
+//   metadata: {}
+// `
+// 	return s
+// }
+
+function getOffSwitchString( a, swString ) {
+	const sw = swString.split( ':' )[ 0 ]
+	const deviceId = getDeviceIdFromName( sw )
+	const conditions = []
+	const time = a.time || 3600
+	const switchUpdatedCondition = `
+  - condition: template
+    value_template: '{{ as_timestamp(now()) - as_timestamp(states.${sw}.last_updated) > ${time} }}'`
+  conditions.push( switchUpdatedCondition )
+	const switchOnCondition = `
+  - condition: template
+    value_template: '{{ states.${sw}.state == "on" }}'`
+  conditions.push( switchOnCondition )
+	const sensorOffConditions = a.sensors.map( ss => {
+		const deviceId = getDeviceIdFromName( ss )
+		const sensorOffCondition = `
+  - condition: template
+    value_template: '{{ as_timestamp(now()) - as_timestamp(states.${ss}.last_updated) > ${time} }}'`
+	  conditions.push( sensorOffCondition )
+	})
+	return conditions.join( '' )
+}
+
+function getSwitchesObject( a, switchesObject ) {
+	return a.switches.reduce( ( acum, sw ) => {
+		const offAutomationSwitchString = getOffSwitchString( a, sw )
+		return {
+			...switchesObject,
+			// todo: dedupe
+			[ sw ]: switchesObject[ sw ] = switchesObject[ sw ] ? ( switchesObject[ sw ] + offAutomationSwitchString ) : offAutomationSwitchString
+		}
+	}, switchesObject )
+}
+
+function getOff( swString ) {
+	const swName = swString.split( ':' )[ 0 ]
+	const friendlyName = swName.split( '.' )[ 1 ]
+	const s = `
+    ${swName}:
+      friendly_name: ${friendlyName}
+      state: 'off'`
+	return s
+}
+
+function getAllOff( switchesObject ) {
+	const id = 'All Off'
+
+	const entities = Object.keys( switchesObject ).map( ( sw ) => {
+		return getOff( sw )
+	}).join( '' )
+
+	const s = `
+- id: '${id}'
+  name: '${id}'
+  entities: ${entities}
+  metadata: {}
+`
+	return s
+}
+
+function getOn( swString ) {
+	const swName = swString.split( ':' )[ 0 ]
+	const friendlyName = swName.split( '.' )[ 1 ]
+	const s = `
+    ${swName}:
+      friendly_name: ${friendlyName}
+      state: 'on'`
+	return s
+}
+
+function getAllOn( switchesObject ) {
+	const id = 'All On'
+
+	const entities = Object.keys( switchesObject ).map( ( sw ) => {
+		return getOn( sw )
+	}).join( '' )
+
+	const s = `
+- id: '${id}'
+  name: '${id}'
+  entities: ${entities}
+  metadata: {}
+`
+	return s
+}
+
+function getOffString( swString, conditions ) {
+	const sw = swString.split( ':' )[ 0 ]
+	const id = `id_off ${sw}`
+	const alias = `Turn off ${sw}`
+	const description = `Turn off ${sw}`
+	const deviceId = getDeviceIdFromName( sw )
+	const trigger = `
+  - platform: event
+    event_type: timer.finished
+    event_data:
+      entity_id: timer.minute`
+  const action = `
+  - type: turn_off
+    device_id: ${deviceId}
+    entity_id: ${sw}
+    domain: ${sw.split('.')[0]}`
+	const s = `
+- id: '${id}'
+  alias: ${alias}
+  description: '${description}'
+  trigger: ${trigger}
+  condition: ${conditions}
+  action: ${action}
+  mode: single
+`
+	return s
+}
+
+function getAutomationString( automations ) {
+	let configurationOutputString = ''
+	let sceneOutputString = ''
+	let switchesObject = {}
+	automations.forEach( a => {
+		if( a.nighttime ) {
+			configurationOutputString += getOnString( a, true, false )
+			configurationOutputString += getOnString( a, false, true )
+		} else {
+			configurationOutputString += getOnString( a, false, false )
+		}
+		switchesObject = getSwitchesObject( a, switchesObject )
+		sceneOutputString += getSceneString( a )
+		if( a.nighttime ) {
+			sceneOutputString += getSceneString( a, a.nighttime )
+		}
+	})
+	configurationOutputString += Object.keys( switchesObject ).map( sw => {
+		const switchesObjectSw = switchesObject[ sw ]
+		return getOffString( sw, switchesObjectSw )
+	}).join( '' )
+
+	sceneOutputString += getAllOff( switchesObject )
+	sceneOutputString += getAllOn( switchesObject )
+
+  writeFileSync( 'automations.yaml', configurationOutputString, {
+    flag: 'w',
+  });
+
+  writeFileSync( 'scenes.yaml', sceneOutputString, {
+    flag: 'w',
+  });
+
+}
+
+getAutomationString( automations )
+
+
